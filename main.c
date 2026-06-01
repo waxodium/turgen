cd4410e2bdfb6e4c517f6526d8b7194f329de7e7
waxodium <waxodium@proton.me>
2026-06-01 11:06:36 +0700

    feat: added arrow keys and history management

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-
 #include <sys/wait.h>
+
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
@@ -13,16 +13,21 @@
 void enableRaw(struct termios *orgTerminal);
 void disableRaw(struct termios *orgTerminal);
 void execute(char *buffer);
+void arrows(char *inputBuffer, int *inputLength, char *prompt, int *totalHistoryCount, int *historyIndex, char historyList[10][1024]);
 
 struct termios cookedTerminal;
 
-int main() {   
+int main() {
     char buffer[1024];
     int position = 0;
     char character;
     char prompt[] = "fash ~> ";
+    char history[10][1024];
+    int count = 0;
+    int historyPosition = -1;
+
     enableRaw(&cookedTerminal);
-    
+
     struct sigaction sa;
     sa.sa_handler = SIG_IGN;
     sigemptyset(&sa.sa_mask);
@@ -44,60 +49,61 @@ int main() {
         }
 
         if (bytes == 0) continue;
-       
-        // Enter key (ASCII 13)
-        if (character == 13) {
-            if (character == 13 || character == 10) {
-                buffer[position] = '\0';
-                
-                sout("\r\n");
-                
-                if (position > 0) {
-                    execute(buffer);
-                }
-                
-                sout("%s", prompt);
-                
-                position = 0;
+
+        switch (character) {
+        case 13:
+            buffer[position] = '\0';
+            sout("\r\n");
+            if (position > 0) {
+                strcpy(history[count % 10], buffer);
+                count++;
+                execute(buffer);
             }
-        } 
-        
-        // Backspace (ASCII 127)
-        else if (character == 127) {
+            sout("%s", prompt);
+            position = 0;
+            break;
+
+        case 127:
             if (position > 0) {
                 position--;
                 sout("\b \b");
             }
-        } 
-            
-        else if (position < 1023) {
-            buffer[position++] = character;
-            sout("%c", character);
+            break;
+
+        case 27:
+            arrows(buffer, &position, prompt, &count, &historyPosition, history);
+            break;
+        
+        case 4:
+            sout("\r\nexit\r\n");
+            disableRaw(&cookedTerminal);
+            exit(0);
+            break;
+
+        default:
+            if (position < 1023) {
+                buffer[position++] = character;
+                sout("%c", character);
+            }
+            break;
         }
-
-
     }
 
     disableRaw(&cookedTerminal);
     return 0;
 }
 
-
 void enableRaw(struct termios *orgTerminal) {
     struct termios rawTerminal;
-
     tcgetattr(STDIN_FILENO, orgTerminal);
     rawTerminal = *orgTerminal;
-
     rawTerminal.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     rawTerminal.c_oflag &= ~(OPOST);
     rawTerminal.c_cflag |= (CS8);
     rawTerminal.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
-
     rawTerminal.c_cc[VMIN] = 0;
     rawTerminal.c_cc[VTIME] = 1;
-
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawTerminal); 
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &rawTerminal);
 }
 
 void disableRaw(struct termios *orgTerminal) {
@@ -118,9 +124,7 @@ void execute(char *buffer) {
     if (argv[0] == NULL) return;
     disableRaw(&cookedTerminal);
 
-    
-    // Fash standard commands
-    if (strcmp(buffer, "exit") == 0) {
+    if (strcmp(argv[0], "exit") == 0) {
         sout("exit\r\n");
         disableRaw(&cookedTerminal);
         exit(0);
@@ -133,20 +137,49 @@ void execute(char *buffer) {
         sigemptyset(&sa.sa_mask);
         sa.sa_flags = 0;
         sigaction(SIGINT, &sa, NULL);
-        
 
         execvp(argv[0], argv);
         if (errno == ENOENT) {
-            sout("\r\nfash: %s: command not found\r\n", buffer);
+            sout("\r\nfash: %s: command not found\r\n", argv[0]);
         }
         exit(1);
-
-
     } else if (pid > 0) {
         wait(NULL);
     }
-
-
     enableRaw(&cookedTerminal);
+}
 
+
+
+void arrows(char *inputBuffer, int *inputLength, char *prompt, int *totalHistoryCount, int *historyIndex, char historyList[10][1024]) {
+    char seq[2];
+    if (read(STDIN_FILENO, &seq[0], 1) <= 0 || read(STDIN_FILENO, &seq[1], 1) <= 0) return;
+    if (seq[0] != '[') return;
+
+    // Up Arrow (A)
+    if (seq[1] == 'A') {
+        if (*historyIndex < *totalHistoryCount - 1 && *historyIndex < 9) {
+            (*historyIndex)++;
+        }
+    } 
+    // Down Arrow (B)
+    else if (seq[1] == 'B') {
+        if (*historyIndex >= 0) {
+            (*historyIndex)--;
+        }
+    } else return;
+
+    sout("\r\033[K%s", prompt);
+
+    if (*historyIndex >= 0) {
+        int target = (*totalHistoryCount - 1 - *historyIndex) % 10;
+        if (target < 0) target += 10; 
+
+        strcpy(inputBuffer, historyList[target]);
+        *inputLength = strlen(inputBuffer);
+        sout("%s", inputBuffer);
+    } else {
+        *inputLength = 0;
+        inputBuffer[0] = '\0';
+    }
 }
